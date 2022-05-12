1b02f31308488ff2d0983b74e3551fb9080a2d91
angelo-marasa <angelo.marasa@kaleidico.com>
2022-05-12 16:35:36 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+/*global chrome*/
+import { useEffect, useState } from "react";
+import "./App.css";
+import Button from "@mui/material/Button";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	//-- State that holds the URL
+	const [url, setUrl] = useState("");
+
+	//-- Use the Chrome API to get the URL of the active tab.
+	useEffect(() => {
+		chrome.tabs.query({ currentWindow: true, active: true }, function(
+			tabs
+		) {
+			//-- Set the URL to the state variable holding the URL
+			setUrl(tabs[0].url);
+		});
+	}, []);
+	return (
+		<div className="App">
+			<h1>Boilerplate!</h1>
+			<Button
+				variant="outlined"
+				onClick={() => {
+					navigator.clipboard.writeText(url);
+				}}
+			>
+				Copy Sharable URL
+			</Button>
+		</div>
+	);
 }
 
 export default App;
